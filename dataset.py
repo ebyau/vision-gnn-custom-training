cbcc4e0fa84d07fa6717c46fbcf6707189b4090c
Brian Ebiyau <38182764+ebyau@users.noreply.github.com>
2024-08-01 18:51:48 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,7 +20,7 @@ def get_data_loaders(train_dir, val_dir, batch_size=8):
         transforms.RandomResizedCrop(224),
         #transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
-        transforms.RandomRotation(10),
+        #transforms.RandomRotation(10),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
